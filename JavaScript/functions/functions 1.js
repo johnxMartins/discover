ce54760befdb8bf2b39b0e1da3c076e73973c249
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-06 15:20:45 -0300

    arrow function

--- JavaScript/functions/functions 1.js
+++ JavaScript/functions/functions 1.js
@@ -2,7 +2,30 @@
 // function anonymous
 // parâmetros (parameters)
 const sum = function (number1, number2) {
-  console.log(number1 + number2)
+  total = number1 + number2 //NAO FAÇA ISSO (DECLARE O TIPO)
+  return total
 }
 
+let number1 = 34
+let number2 = 24
 sum(2, 3) //arguments - argumentos
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`)
+console.log(total)
+
+/*
+// function hosting
+sayMyName()
+
+function sayMyName() {
+  console.log('John')
+}
+*/
+
+// arrow Function
+const sayMyName = () => {
+  console.log('Nome')
+}
+sayMyName()
