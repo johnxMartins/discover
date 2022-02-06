4d82c1625de76c6cb43640466805448e3db7efa7
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-06 14:13:11 -0300

    functions

---
+++ JavaScript/functions/functions 1.js
@@ -0,0 +1,8 @@
+// function expression
+// function anonymous
+// parâmetros (parameters)
+const sum = function (number1, number2) {
+  console.log(number1 + number2)
+}
+
+sum(2, 3) //arguments - argumentos
---
+++ JavaScript/functions/functions.js
@@ -0,0 +1,13 @@
+// Criar app de frases motivacionais
+
+// declaration - declaração da função
+// function statement
+function createPhrases() {
+  console.log('Criar frases')
+  console.log('Estudar é muito bom')
+  console.log('Paciencia e a mae da perscistencia')
+}
+
+// executar a função / rodar, chamar, invocar
+// execute, run, call, invoke
+createPhrases()
