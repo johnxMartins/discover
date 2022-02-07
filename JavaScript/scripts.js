acb9ba3d5dcdfdee4359b491554adb32dcdb387a
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-07 20:58:42 -0300

    strings

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -4,3 +4,10 @@
   -prototype chain
   - __proto__
 */
+
+// Strings e Numeros
+// transformar string em numero e vice-versa
+let string = '123'
+console.log(Number(string))
+let number = 321
+string(number)
