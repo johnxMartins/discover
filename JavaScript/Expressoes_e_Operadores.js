b73231e7a17b2a3b249a22e74da2ebffae9576e4
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-08 13:49:09 -0300

    operadores

---
+++ JavaScript/Expressoes_e_Operadores.js
@@ -0,0 +1,6 @@
+// Expressoes e Operadores
+// Operators: Binary, Unary e Ternary
+let number = 1
+console.log(number + 1) //Binary
+console.log(number--) //Unary
+console.log(true ? 'alo' : 'nada') //Ternary
