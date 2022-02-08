053a5f001df1c28db8d894ef4e49c0608af7ce18
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-08 14:44:26 -0300

    Falsy e truthy

--- JavaScript/Expressoes_e_Operadores.js
+++ JavaScript/Expressoes_e_Operadores.js
@@ -132,3 +132,32 @@ console.log(niceBreakfast)
 // concatenation (retorna a união de 2 strings)
 let alpha = 'alpha'
 console.log(alpha + 'bet' + 's')
+
+// Falsy e truthy
+// Falsy: quando um valor é considerado false em contextos onde um booleano é obrigatório (condicionais e loops)
+/*
+	Todos os valores abaixo seriam representados como false em um boolean.
+		false
+    0
+    -0
+    ""
+    null
+    undefined
+    NaN
+*/
+console.log(NaN ? 'verdadeiro' : 'falso')
+// Truthy: quando um valor é considerado true em contextos onde um booleano é obrigatório.
+/* 
+	Todos os valores abaixo seriam representados como false em um boolean.
+		true
+    {}
+    []
+    1
+    3.23
+    "0"
+    "false"
+    -1
+    Infinity
+    -Infinity
+*/
+console.log(Infinity ? 'verdadeiro' : 'falso')
