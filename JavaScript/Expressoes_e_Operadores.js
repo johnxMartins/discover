9f07e6560267ebe95a0bc1efea3f9e946f1fcf8e
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-08 14:03:08 -0300

    Operadores aritmeticos

--- JavaScript/Expressoes_e_Operadores.js
+++ JavaScript/Expressoes_e_Operadores.js
@@ -19,3 +19,27 @@ const person = {
 }
 delete person.age // Procura uma propiedade e se existir deleta
 console.log(person)
+
+// Operadores Aritméticos
+// Multiplicação '*'  // Divisão '/'
+// Soma '+'           // Subtração '-'
+// Resto da divisão  '%'
+// Incremento e Decremento ('++' '--')
+/* resto da divisão: sinal % */
+let remainder
+remainder = 11 % 9
+console.log(remainder)
+
+/* incremento: sinal ++ */
+let increment = 0
+
+console.log(++increment)
+console.log(increment)
+
+/* decremento: sinal -- */
+let decrement = 0
+decrement--
+console.log(decrement)
+
+/* exponencial: sinal ** */
+console.log(2 ** 3)
