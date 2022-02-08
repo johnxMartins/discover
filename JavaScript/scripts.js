f6a4b44029cb0bd95046fc073ff6286433015b15
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-07 21:05:28 -0300

    casas decimais

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -14,8 +14,18 @@ let number = 321
 string(number)
 */
 
+/*
 // Contando caracteres e dígitos
 let word = 'Paralelepipedo'
 console.log(word.length)
 let number = 1234
 console.log(String(number).length)
+*/
+
+// Casas decimais
+// transformar numero quebrado com 2 casas decimais e trocar ponto por virgula
+
+let number = 21321312.231231
+console.log(number.toFixed(2))
+console.log(number.replace('.', ','))
+console.log(number)
