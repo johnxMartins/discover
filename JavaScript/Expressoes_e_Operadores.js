03bb30df40f090422513609c45e2a9dd429e1f10
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-08 13:55:03 -0300

    TypeOf

--- JavaScript/Expressoes_e_Operadores.js
+++ JavaScript/Expressoes_e_Operadores.js
@@ -11,3 +11,11 @@ let name = newString('John')
 name.surName = 'Brito'
 let date = new Date('2020-12-01')
 console.log(date.__proto__)
+
+// TypeOf delete
+const person = {
+  name: 'John',
+  age: 22
+}
+delete person.age // Procura uma propiedade e se existir deleta
+console.log(person)
