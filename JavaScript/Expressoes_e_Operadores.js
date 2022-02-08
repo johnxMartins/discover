9207d71f31b00d29602c58bc16d84009d0414d93
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-08 13:52:20 -0300

    new

--- JavaScript/Expressoes_e_Operadores.js
+++ JavaScript/Expressoes_e_Operadores.js
@@ -4,3 +4,10 @@ let number = 1
 console.log(number + 1) //Binary
 console.log(number--) //Unary
 console.log(true ? 'alo' : 'nada') //Ternary
+
+//  New (left-hand-side expression)
+// Criar um novo Objeto
+let name = newString('John')
+name.surName = 'Brito'
+let date = new Date('2020-12-01')
+console.log(date.__proto__)
