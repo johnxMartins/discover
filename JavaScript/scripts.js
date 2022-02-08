36f925d842dd0f2c92c1368b90d5f58028226255
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-07 21:02:59 -0300

    contando caracteres

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -7,7 +7,15 @@
 
 // Strings e Numeros
 // transformar string em numero e vice-versa
+/*
 let string = '123'
 console.log(Number(string))
 let number = 321
 string(number)
+*/
+
+// Contando caracteres e dígitos
+let word = 'Paralelepipedo'
+console.log(word.length)
+let number = 1234
+console.log(String(number).length)
